0c4649c7d3f1b33794f05c0e470163be6c0e9269
naimuddin01 <103277035+naimuddin01@users.noreply.github.com>
2022-09-20 23:51:57 +0600

    finished

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const express = require('express')
 const cors = require('cors')
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);//for payment
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -48,6 +50,7 @@ async function run() {
     const bookingCollection = client.db('doctors-portal').collection('bookings');
     const userCollection = client.db('doctors-portal').collection('users');
     const doctorCollection = client.db('doctors-portal').collection('doctors');
+    const paymentCollection = client.db('doctors-portal').collection('payments');
 
 
     const verifyAdmin = async (req, res, next) => {
@@ -61,6 +64,21 @@ async function run() {
       }
     }
 
+    //user for payment intention 
+    app.post('/create-payment-intent',verifyJWT, async (req, res) => {
+      const service = req.body
+      const price = service.price;
+      const amount = price*100;
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency : 'usd',
+        payment_method_types: ['card']
+
+      });
+      res.send({clientSecret: paymentIntent.client_secret});
+    })
+    //user for payment intention 
 
     app.get('/service', async (req, res) => {
       const query = {}
@@ -232,6 +250,13 @@ async function run() {
 
     })
 
+    app.get('/booking/:id', verifyJWT, async(req, res) => {
+      const id = req.params.id;
+      const query ={_id: ObjectId(id)}
+      const booking = await bookingCollection.findOne(query);
+      res.send(booking);
+    })
+
     //Mongo te Booking er information pathanor jonno
     app.post('/booking', async (req, res) => {
       const booking = req.body;
@@ -248,7 +273,30 @@ async function run() {
       return res.send({ success: true, result }); //(return) word ta dile o hoy na dile o hoy
     })
 
+    app.patch('/booking/:id', verifyJWT, async (req, res) => {
+      const id =req.params.id;
+      const payment = req.body
+      const filter = {_id: ObjectId(id)};
+      
+      //maybe
+      //importent
+      //updateDoc holo amra jothy database thake kono data pathate ba (ante parbo kina sure na) chi and sei jonno jothy kno sotto thake tahole updateDov use kori
+      const updatedDoc = {
+        $set:{
+          //paid jothy true hoy tahole taransactionId ta database e pathabo
+          paid: true,
+          transactionId: payment.transactionId,
 
+        }
+      }
+
+      //paymentCollection e notun kore data bosasce
+      const result = await paymentCollection.insertOne(payment)
+
+      //bookingCollection e data update kortece
+      const updatedBooking = await bookingCollection.updateOne(filter, updatedDoc);
+      res.send(updatedDoc);
+    })
 
     app.get('/doctor', verifyJWT, verifyAdmin, async (req, res) => {
       const doctor = await doctorCollection.find().toArray();
